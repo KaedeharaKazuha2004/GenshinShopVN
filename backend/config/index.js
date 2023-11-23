f5ec94f9beb7485b803eb794904d835d6469eac8
B2207567-VUDUCTHANG <thangB2207567@student.ctu.edu.vn>
2023-11-24 06:56:43 +0700

    Do an CT449

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,6 @@
 const config = {
 	db: {
-		string: process.env.DB_CONNECT_STRING || "mongodb://localhost:27017/QLyBanHang",
+		string: process.env.DB_CONNECT_STRING || "mongodb://localhost:27017/GenshinShopVN",
 	},
 	jwt: {
 		ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || "access_token",
