dbba33ac9daeef8a7880861b7b613611a472c592
B2207567-VUDUCTHANG <thangB2207567@student.ctu.edu.vn>
2023-11-24 07:05:24 +0700

    Do an CT449

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,6 @@
 const config = {
 	db: {
-		string: process.env.DB_CONNECT_STRING || "mongodb://localhost:27017/GenshinShopVN",
+		string: process.env.DB_CONNECT_STRING || "mongodb://127.0.0.1:27017/GenshinShopVN",
 	},
 	jwt: {
 		ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || "access_token",
